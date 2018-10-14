f437a2b1e7534fccd149370733a1108a5b9864a0
HALABI Sami <sami.halabi.etu@univ-lille.fr>
2018-10-14 11:02:18 +0200

    revert get

--- getch.py
+++ getch.py
@@ -1,46 +1,43 @@
-# import sys,tty,termios
+import sys,tty,termios
 from threading import Thread, RLock
-from pynput import keyboard
-
-verrou = RLock()
-
-#class _Getch:
-#     def getch(self):
-#             fd = sys.stdin.fileno()
-#             old_settings = termios.tcgetattr(fd)
-#             try:
-#                 tty.setraw(sys.stdin.fileno())
-#                 ch = sys.stdin.read(1)
-#             finally:
-#                 termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-#             return ch
-
-class KeyListener(Thread):
-
-    def __init__(self, avatar):
+ verrou = RLock()
+ class _Getch:
+    def __call__(self):
+            fd = sys.stdin.fileno()
+            old_settings = termios.tcgetattr(fd)
+            try:
+                tty.setraw(sys.stdin.fileno())
+                ch = sys.stdin.read(3)
+            finally:
+                termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+            return ch
+ class KeyListener(Thread):
+     def __init__(self, avatar):
         Thread.__init__(self)
         self.avatar = avatar
-        # self.inkey = _Getch()
-
-    def get(self, k):
-        if k == keyboard.Key.up:
-                self.change(1)
-        elif k == keyboard.Key.down:
-                self.change(0)
-        elif k == keyboard.Key.right:
-                self.change(3)
-        elif k == keyboard.Key.left:
-                self.change(2)
-        else:
-                pass
-        #         sys.exit(0)
-
-    def change(self, val):
+     def get(self):
+            inkey = _Getch()
+            while(1):
+                    k=inkey()
+                    if k!='':break
+            if k=='\x1b[A':
+                    print("up")
+                    self.change(1)
+            elif k=='\x1b[B':
+                    print("down")
+                    self.change(0)
+            elif k=='\x1b[C':
+                    print("right")
+                    self.change(3)
+            elif k=='\x1b[D':
+                    print ("left")
+                    self.change(2)
+            else:
+                    print("Bye")
+                    sys.exit(0)
+     def change(self, val):
         with verrou:
             self.avatar.on_press(val)
-
-    def run(self):
-            with keyboard.Listener(on_press=self.get) as listener:
-                listener.join()
-#         while(1):
-#             self.get()
+     def run(self):
+        while(1):
+            self.get()
